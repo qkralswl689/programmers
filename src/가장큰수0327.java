44dd6863bee3a5c6965b2fd2661908b4c185a1c1
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-03-27 20:42:21 +0900

    가장큰수

---
+++ src/가장큰수0327.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class 가장큰수0327 {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {6,10,2};
+
+        String[] tmp = new String[numbers.length];
+
+        for(int i = 0; i < numbers.length; i++ ){
+            tmp[i] = String.valueOf(numbers[i]);
+        }
+
+        String[] answertmp = new String[numbers.length * numbers.length];
+        for (int i = 0; i < numbers.length; i++){
+
+            for(int j = 1; j <= numbers.length; j++ ){
+
+
+            }
+        }
+    }
+}
--- src/숫자게임0324.java
+++ src/숫자게임0324.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class 숫자게임0324 {
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 숫자게임0324 {
+    public static void main(String[] args) {
+
+
+        // 정확성 76.2/ 효율성 14.3
+        int[] A = {5,1,3,7};
+
+        int[] B = {2,2,6,8};
+
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        for(int i = 0; i < A.length; i++){
+            if(B[i] >= A[answer]){
+                answer++;
+            }else {
+                continue;
+            }
+        }
+
+        System.out.println(answer);
+       /* // 정확성 23.8/ 효율성 0 -> 시간초과
+        int[] A = {5,1,3,7};
+
+        int[] B = {2,2,6,8};
+
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        for(int i = A.length -1; i >= 0; i--){
+            if(A[i] < B[i]){
+                answer++;
+
+            }
+        }
+
+
+        System.out.println(answer);*/
+
+
+
+
+
+        // 정확성 23.8/ 효율성 0 -> 시간초과
+       /* int[] A = {5,1,3,7};
+
+        int[] B = {2,2,6,8};
+
+        int answer = 0;
+
+        for(int i = 0; i < A.length; i++){
+            for(int j = 0; j < B.length; j++){
+                if(A[i] < B[j] && B[j] != 0){
+                    B[j] = 0;
+                    answer++;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(answer);*/
+    }
 }
