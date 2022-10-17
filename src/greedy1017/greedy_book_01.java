e1d01ad5babc4b16df9d0c23acb5816251a6ab91
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-10-17 23:33:49 +0900

    greedy

---
+++ src/greedy1017/greedy_book_01.java
@@ -0,0 +1,26 @@
+package greedy1017;
+
+import java.util.List;
+
+public class greedy_book_01 {
+    public static void main(String[] args) {
+
+        // 1,260 을 500/100/50/10 원으로 거슬러줄때 동전의 최소 개수
+
+        int money = 1260;
+        int count = 0;
+        int[] coin = {500,100,50,10};
+
+        for(int i = 0; i < coin.length; i ++){
+            System.out.println("before" + money + "\n");
+            count += money / coin[i];
+            money %= coin[i];
+            System.out.println("after" + money+ "\n");
+//            if(money / coin[i] < 0){
+//                break;
+//            }
+
+        }
+        System.out.println("count : " + count);
+    }
+}
---
+++ src/greedy1017/greedy_book_02.java
@@ -0,0 +1,43 @@
+package greedy1017;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class greedy_book_02 {
+    public static void main(String[] args) {
+
+        int n = 5;
+        int m = 8;
+        int k = 3;
+
+        int result = 0;
+        Integer[] array = {2,4,5,4,6};
+        Arrays.sort(array, Collections.reverseOrder());
+
+        for (int i = 0; i < m; i ++){
+
+            for(int j = 0; j < k; j++){
+
+                result += array[0];
+                i++;
+            }
+            result += array[1];
+
+        }
+
+        //// 책 답
+
+        Arrays.sort(array);
+        int first = array[n-1]; // 첫번째로 큰 수
+        int second = array[n-2]; // 두번째로 큰 수
+
+        // 가장 큰 수가 더해지는 횟수 계산
+        int cnt = (m / (k+1)) * k;
+        cnt += m % (k+1);
+
+        int answer = 0;
+        answer += cnt * first; // 가장 큰 수 더하기
+        answer += (m-cnt) + second; //두번째로 큰 수 더하기
+
+    }
+}
