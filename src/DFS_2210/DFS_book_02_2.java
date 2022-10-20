a109883e283e12a3707f9ff903e78e5a5f82405d
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-10-20 23:56:01 +0900

    again DFS

---
+++ src/DFS_2210/DFS_book_02_2.java
@@ -0,0 +1,57 @@
+package DFS_2210;
+
+import java.util.Scanner;
+
+public class DFS_book_02_2 {
+
+    public static int[][] ice;
+    public static int n , m;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        ice = new int[n][m];
+
+        for(int i = 0; i < n; i ++){
+             for(int j = 0; j < m; j++){
+                 int x = sc.nextInt();
+                 ice[i][j] = x;
+             }
+        }
+
+        int result = 0;
+
+        for(int i = 0; i < n; i ++){
+            for(int j = 0; j < m; j++){
+                if(def(i,j)){
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+
+    public static boolean def(int x , int y){
+        if(x <= -1 && x >= n || y <= -1 && y >= m){
+            return false;
+        }
+
+        if(ice[x][y] == 0){
+            ice[x][y] = 1;
+
+            def(x-1,y);
+            def(x,y+1);
+            def(x+1,y);
+            def(x,y-1);
+
+
+            return true;
+        }
+
+        return false;
+    }
+}
