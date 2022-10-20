c9a2749732b447465a3ecbdd96182176897256b2
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-10-21 00:11:02 +0900

    understand DFS

--- src/DFS_2210/DFS_book_02_2.java
+++ src/DFS_2210/DFS_book_02_2.java
@@ -36,13 +36,16 @@ public class DFS_book_02_2 {
     }
 
     public static boolean def(int x , int y){
-        if(x <= -1 && x >= n || y <= -1 && y >= m){
+
+
+        if(x <= -1 || x >= n || y <= -1 || y >= m){
             return false;
         }
 
         if(ice[x][y] == 0){
             ice[x][y] = 1;
 
+            // 상하 좌우 
             def(x-1,y);
             def(x,y+1);
             def(x+1,y);
