99236d59f7634f7d42ec8799ba5966d7d46d55a1
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-11-04 23:55:45 +0900

    eratos try

---
+++ src/eratosthenes/baekjoon_1929.java
@@ -0,0 +1,102 @@
+package eratosthenes;
+
+import java.util.Scanner;
+
+public class baekjoon_1929 {
+    public static int n, m;
+    public static boolean[] num ;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        num = new boolean[m + 1];
+
+        num[0] = true;
+        num[1] = true;
+        //Math.sqrt() 제곱근 구하는 함수
+        for(int i = n; i <=m; i++){
+            int result = 0;
+            int x = (int)Math.sqrt(i);
+
+            if( i == 1){
+                result++;
+            }
+
+            for(int j = 2; j < x; j++){
+                if(i % j == 0){
+                    result++;
+                    break;
+                }
+            }
+
+            if(result == 0){
+                System.out.println(i);
+            }
+        }
+
+         }
+        // 시간초과 (동빈나)
+//        for(int i = 0; i <= m ; i++ ){
+//            num[i] = i ;
+//        }
+//
+//
+//        for(int i = n; i <= m; i++){
+//            era(i);
+//            if(num[i] != 0){
+//                System.out.println(num[i]);
+//            }
+//        }
+//    }
+//
+//    public static void era(int number){
+//
+//        for(int i = 2; i < number; i++){
+//            if(num[i-1] == 0){
+//                continue;
+//            }else{
+//                for(int j = i + i ; j <= number; j+= i){
+//                    num[j] = 0;
+//                }
+//            }
+//        }
+//
+//    }
+    // 시간초과
+//    public static int n, m;
+//
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        n = sc.nextInt();
+//        m = sc.nextInt();
+//
+//        for (int i = n; i < m; i++) {
+//
+//            if (era(i) == true) {
+//                System.out.println(i);
+//            }
+//
+//        }
+//
+//    }
+//
+//    public static boolean era(int number){
+//
+//        if(number < 2){
+//            return false;
+//        }
+//
+//        for(int i = 2; i < number; i++){
+//            if(number % i == 0){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+
+
+
+}
---
+++ src/eratosthenes/baekjoon_2609.java
@@ -0,0 +1,8 @@
+package eratosthenes;
+
+public class baekjoon_2609 {
+
+    public static void main(String[] args) {
+
+    }
+}
